635454c2b6c9d5a20949570c93f8a82eb4e406ca
Oliwer Helsén <oliwer.helsen@live.com>
2013-03-05 23:46:32 +0100

    yeah

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include('templates/initial.tpl.php');
 $html = array();
 
 $html['head'] = htmlHead();
-$html['initial'] = htmlInitial();
+$html['initial'] = htmlPage();
 $html['foot'] = htmlFoot();
 
 print(implode($html));
